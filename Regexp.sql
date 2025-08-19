0a9705a579e5db2d8150165bf1b7e914ac138dee
Pranavster <122951073+Pranavster@users.noreply.github.com>
2025-08-18 20:50:59 -0500

    Removing unnecessary files

--- Regexp.sql
+++
@@ -1,9 +0,0 @@
-SELECT REGEXP_INSTR("abc", '^[0-9]');
-SELECT REGEXP_INSTR("781", '^[0-9]');
-SELECT REGEXP_INSTR("!@#", '^[0-9]') = 0;
-select regexp_instr("8234567890", '[^0-9]');
-
-
-select * from gas_stn.supply_order;
-
-desc gas_stn.supply_order;
